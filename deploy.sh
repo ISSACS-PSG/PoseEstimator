1db1c31c9ebcf7b9aa6b4674410ac5b87f5b8d8c
Chris Nurre <cnurre@icloud.com>
2022-05-27 12:33:09 -0400

    Moving everything into a src folder for easier syncing

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,4 @@ aws cloudformation deploy --template-file aws-cf-template.yaml --stack-name $STA
 # Retrieve the outputs and save as variables to use later
 eval `aws cloudformation describe-stacks --query Stacks[].Outputs[*].[OutputKey,OutputValue] --stack-name $STACK_NAME | jq -r '.[][] | "export \(.[0])=\(.[1])"'`
 
-aws s3 cp sketch.js s3://$S3BucketName
-aws s3 cp index.html s3://$S3BucketName
-aws s3 cp index.css s3://$S3BucketName
+aws s3 sync ./src/ s3://$S3BucketName

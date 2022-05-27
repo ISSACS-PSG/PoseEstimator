a9218347dc63611f834960b165c5b91acfb6a29c
Chris Nurre <cnurre@icloud.com>
2022-05-27 15:54:50 -0400

    Cleanup

--- src/sketch.js
+++ src/sketch.js
@@ -6,26 +6,42 @@ let scoreThreshold = 0.5;
 let offset = {x: 0, y: 0};
 let aspectFillScale = 1;
 
+let logging = false;
+let loggingButton;
+let downloadButton;
 
 async function setup() {
     detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet);
-
-    createCanvas(windowWidth, windowHeight);
-    video = createCapture(VIDEO, videoReady);
+    video = await createCapture(VIDEO, videoReady);
 
     // Hide the video element, and just show the canvas
     video.hide();
-}
-
-function videoReady() {
-    console.log("video ready");
-    frameRate(updateRate);
     
-    getPoses();
+    createCanvas(windowWidth, windowHeight);
+    
+    loggingButton = createButton('Start');
+    loggingButton.mousePressed(toggleLogging);
+    
+    downloadButton = createButton('Download');
+    downloadButton.mousePressed(downloadCSV);
+    
+    updateButtons();
+    downloadButton.hide();
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);  
+    resizeCanvas(windowWidth, windowHeight); 
+    updateButtons(); 
+}
+
+function videoReady() {
+    while (!video.loadedmetadata) {}
+    
+    console.log("video ready");
+    
+    frameRate(updateRate);
+    
+    getPoses();
 }
 
 async function getPoses() {
@@ -33,8 +49,9 @@ async function getPoses() {
     setTimeout(getPoses, (1/updateRate) * 1000.0);
 }
 
+
+// Drawing
 function draw() {
-    
     if (video === undefined) {
         return;
     }
@@ -56,9 +73,10 @@ function draw() {
     // We can call both functions to draw all keypoints and the skeletons
     drawKeypoints();
     drawSkeleton();
+    
+    logPoints();
 }
 
-// A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
   // Loop through all the poses detected
   for (let i = 0; i < poses.length; i += 1) {
@@ -67,7 +85,7 @@ function drawKeypoints() {
     for (let j = 0; j < pose.keypoints.length; j += 1) {
       // A keypoint is an object describing a body part (like rightArm or leftShoulder)
       const keypoint = pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
+      // Only draw an ellipse is the pose probability is bigger than threshold
       if (keypoint.score > scoreThreshold) {
         fill(255, 0, 0);
         stroke(255, 255, 255);
@@ -79,9 +97,8 @@ function drawKeypoints() {
   }
 }
 
-// A function to draw the skeletons
 function drawSkeleton() {
-    // Loop through all the skeletons detected
+    // Loop through all the poses detected
     for (let i = 0; i < poses.length; i += 1) {
         const pose = poses[i];
         poseDetection.util.getAdjacentPairs("MoveNet").forEach(([i, j]) => {
@@ -99,4 +116,89 @@ function drawSkeleton() {
         
         return; // Only draw one pose
     }
+}
+
+
+// Logging
+let loggedPoints = [];
+
+function updateButtons() {
+    const padding = 20;
+    const buttonWidth = windowWidth - (2 * padding);
+    const buttonHeight = min(100, (windowHeight * 0.1));
+    
+    loggingButton.size(buttonWidth, buttonHeight);
+    loggingButton.position(padding, windowHeight - padding - buttonHeight);
+    
+    downloadButton.size(buttonWidth, buttonHeight);
+    downloadButton.position(padding, padding);
+    
+    if (logging) {
+        downloadButton.hide();
+    } else {
+        downloadButton.show();
+    }
+}
+
+function toggleLogging() {    
+    if (!logging) {
+        loggedPoints = [];
+        loggingButton.html("Stop");
+    } else {
+        loggingButton.html("Start");
+    }
+    
+    logging = !logging;
+    
+    updateButtons();
+}
+
+function logPoints() {
+    if (!logging) {
+        return;
+    }
+    
+    let timestamp = luxon.DateTime.now().toISO();
+    
+    for (let i = 0; i < poses.length; i += 1) {
+      // For each pose detected, loop through all the keypoints
+      const pose = poses[i];
+      for (let j = 0; j < pose.keypoints.length; j += 1) {
+        // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+        const keypoint = pose.keypoints[j];
+        // Only log points above the threshold
+        if (keypoint.score > scoreThreshold) {
+            loggedPoint = {time: timestamp,
+                           name: keypoint.name,
+                           x: keypoint.x,
+                           y: keypoint.y, 
+                           score: keypoint.score};
+            loggedPoints.push(loggedPoint);
+        }
+      }
+
+      return; // Only draw one pose
+    }
+}
+
+function convertToCSV(arr) {
+  const array = [Object.keys(arr[0])].concat(arr)
+
+  return array.map(it => {
+    return Object.values(it).toString()
+  }).join('\n')
+}
+
+function downloadCSV() {
+    var csvString = convertToCSV(loggedPoints);
+    var csvBlob = new Blob([csvString]);
+    
+    var a = window.document.createElement("a");
+    a.href = window.URL.createObjectURL(csvBlob, {
+        type: "text/plain"
+    });
+    a.download = "data.csv";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 }

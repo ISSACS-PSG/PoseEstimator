33b9742b18386f9aa0d03cc22b0aa94b18db9ae7
Chris Nurre <cnurre@icloud.com>
2022-05-27 10:06:20 -0400

    Only display the first pose

--- sketch.js
+++ sketch.js
@@ -60,39 +60,43 @@ function draw() {
 
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
-  // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i += 1) {
-    // For each pose detected, loop through all the keypoints
-    const pose = poses[i];
-    for (let j = 0; j < pose.keypoints.length; j += 1) {
-      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
-      const keypoint = pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
-      if (keypoint.score > scoreThreshold) {
-        fill(255, 0, 0);
-        noStroke();
-        ellipse((keypoint.x * aspectFillScale) + offset.x, (keypoint.y * aspectFillScale) + offset.y, 10, 10);
-      }
+    if (poses.length == 0) {
+        return;
     }
-  }
+    
+    // only show points for the first pose
+    const pose = poses[0];
+    for (let j = 0; j < pose.keypoints.length; j += 1) {
+        // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+        const keypoint = pose.keypoints[j];
+        // Only draw an ellipse is the pose probability is bigger than 0.2
+        if (keypoint.score > scoreThreshold) {
+            fill(255, 0, 0);
+            stroke(255, 255, 255);
+            ellipse((keypoint.x * aspectFillScale) + offset.x, (keypoint.y * aspectFillScale) + offset.y, 10, 10);
+        }
+    }
 }
 
 // A function to draw the skeletons
 function drawSkeleton() {
-    // Loop through all the skeletons detected
-    for (let i = 0; i < poses.length; i += 1) {
-        const pose = poses[i];
-        poseDetection.util.getAdjacentPairs("MoveNet").forEach(([i, j]) => {
-            const kp1 = pose.keypoints[i];
-            const kp2 = pose.keypoints[j];
-
-            // If score is null, just show the keypoint.
-            const score1 = kp1.score != null ? kp1.score : 1;
-            const score2 = kp2.score != null ? kp2.score : 1;
-            if (score1 >= scoreThreshold && score2 >= scoreThreshold) {
-              stroke(255, 0, 0);
-              line((kp1.x * aspectFillScale) + offset.x, (kp1.y * aspectFillScale) + offset.y, (kp2.x * aspectFillScale) + offset.x, (kp2.y * aspectFillScale) + offset.y);
-            }
-        });
+    if (poses.length == 0) {
+        return;
     }
+    
+    // only build the skeleton for the first pose
+    const pose = poses[0];
+    
+    poseDetection.util.getAdjacentPairs("MoveNet").forEach(([i, j]) => {
+        const kp1 = pose.keypoints[i];
+        const kp2 = pose.keypoints[j];
+
+        // If score is null, just show the keypoint.
+        const score1 = kp1.score != null ? kp1.score : 1;
+        const score2 = kp2.score != null ? kp2.score : 1;
+        if (score1 >= scoreThreshold && score2 >= scoreThreshold) {
+          stroke(255, 0, 0);
+          line((kp1.x * aspectFillScale) + offset.x, (kp1.y * aspectFillScale) + offset.y, (kp2.x * aspectFillScale) + offset.x, (kp2.y * aspectFillScale) + offset.y);
+        }
+    });
 }
